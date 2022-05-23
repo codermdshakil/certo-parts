e6908645d654ba6060dc2e81d4ce03e50132b30c
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-23 22:55:14 +0600

    business summery section icon chagnes

--- src/Pages/Home/BSummery/BSummery.js
+++ src/Pages/Home/BSummery/BSummery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './BSummery.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFlag, faBook, faUsers, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faMoneyCheckDollar ,faScrewdriverWrench ,faUsers, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 const BSummery = () => {
     return (
@@ -13,24 +13,24 @@ const BSummery = () => {
             <div className='bSummery-container mt-10'>
                 <div className='bSummery-Content   grid lg:grid-cols-4  md:grid-cols-2 grid-cols-1 md:px-20'>
                     <div className='mx-auto  mb-10  text-center'>
-                        <FontAwesomeIcon className='text-6xl mb-5 text-secondary' icon={faFlag}></FontAwesomeIcon>
-                        <h1 className='text-4xl text-primary font-bold'>72</h1>
-                        <h3 className='text-2xl font-semibold  mb-3'>Countries</h3>
+                        <FontAwesomeIcon className='text-6xl mb-5 text-secondary' icon={faUsers}></FontAwesomeIcon>
+                        <h1 className='text-4xl text-primary font-bold'>100+</h1>
+                        <h3 className='text-2xl font-semibold  mb-3'>Customers</h3>
                     </div>
                     <div className='mx-auto mb-10  text-center'>
-                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faBook}></FontAwesomeIcon>
-                        <h1 className='text-4xl text-primary font-bold'>523+</h1>
-                        <h3 className='text-2xl font-semibold mb-3'>Complete Project's</h3>
+                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faMoneyCheckDollar}></FontAwesomeIcon>
+                        <h1 className='text-4xl text-primary font-bold'>120M+</h1>
+                        <h3 className='text-2xl font-semibold mb-3'> Annual Revenue</h3>
                     </div>
                     <div className='mx-auto mb-10  text-center'>
-                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faUsers}></FontAwesomeIcon>
-                        <h1 className='text-4xl text-primary font-bold'>273+</h1>
-                        <h3 className='text-2xl font-semibold mb-3'>Happy Clients</h3>
+                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faThumbsUp}></FontAwesomeIcon>
+                        <h1 className='text-4xl text-primary font-bold'>35k+</h1>
+                        <h3 className='text-2xl font-semibold mb-3'>Reviews</h3>
                     </div>
                     <div className='mx-auto  mb-10 text-center'>
-                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faThumbsUp}></FontAwesomeIcon>
-                        <h1 className='text-4xl text-primary font-bold'>432+</h1>
-                        <h3 className='text-2xl font-semibold mb-3'>Feedbacks</h3>
+                        <FontAwesomeIcon className='text-6xl mb-5  text-secondary' icon={faScrewdriverWrench}></FontAwesomeIcon>
+                        <h1 className='text-4xl text-primary font-bold'>500+</h1>
+                        <h3 className='text-2xl font-semibold mb-3'>Tools</h3>
                     </div>
                 </div>
             </div>
