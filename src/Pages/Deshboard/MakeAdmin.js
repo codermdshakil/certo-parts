359478ce9eade74a738cbcacc5cef9c3b280af8d
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-26 12:07:14 +0600

    make admin route added

--- src/Pages/Deshboard/MakeAdmin.js
+++ src/Pages/Deshboard/MakeAdmin.js
@@ -4,12 +4,13 @@ import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Spinner from '../../Shared/Spinner';
+import UserRow from './UserRow';
 
 const MakeAdmin = () => {
 
     const navigate = useNavigate();
 
-    const { data: allusers, isLoading } = useQuery('users', () =>
+    const { data: allusers, isLoading , refetch} = useQuery('users', () =>
         fetch(`https://secret-reaches-23415.herokuapp.com/users`, {
             method: 'GET',
             headers: {
@@ -36,6 +37,29 @@ const MakeAdmin = () => {
     return (
         <div>
             <h1>All users is here{allusers?.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Make Admin</th>
+                            <th>Remove User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allusers?.map((user, index) => <UserRow
+                            key={user?._id}
+                            user={user}
+                            index={index}
+                            refetch={refetch}
+                            ></UserRow>)
+                        }    
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
---
+++ src/Pages/Deshboard/UserRow.js
@@ -0,0 +1,47 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, index, refetch }) => {
+
+    const { email, role } = user;
+
+    const navigate = useNavigate();
+
+    const handleMakeAdmin = () => {
+        fetch(`http://localhost:5000/users/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    localStorage.removeItem('accessToken')
+                    signOut(auth);
+                    return navigate('/');
+                }
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                if (data?.modifiedCount > 0) {
+                    toast.success('Successfully Made an Admin')
+                    refetch()
+                }
+            })
+    }
+
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{email}</td>
+            <td>{role ? <button onClick={handleMakeAdmin} class="btn btn-xs bg-green-500 border-0 text-white">Already Admin</button> : <button onClick={handleMakeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button className='btn btn-error btn-xs'> Remove User</button></td>
+        </tr>
+    );
+};
+
+export default UserRow;
